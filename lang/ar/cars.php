3cf883d7d96c5eb219651112e6033dde0396b1c1
Engy-Mohamed <e.mohamed1984eg@gmail.com>
2024-08-24 02:39:58 +0300

    day 14 :adding multilingual feature

---
+++ lang/ar/cars.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+
+
+    'addHeading' => 'اضافة سيارة جديدة',
+    'titleLabel' => 'عنوان السيارة',
+    'priceLabel' => 'السعر',
+    'descriptionLabel' => 'الوصف',
+    'imageLabel' => 'صورة السيارة',
+    'publishedLabel' => 'للنشر',
+    'categoryLabel' => 'الفئة',
+    'addBtn' => 'اضافة سيارة',
+    'selectCategory' => 'اختار الفئة',
+    'titlePlaceHolder' => 'بى ام دبيو',
+    'pricePlaceHolder' => 'ادخل السعر',
+
+
+];
--- routes/admin.php
+++ routes/admin.php
@@ -2,13 +2,21 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\carController;
 
-Route::get('cars/create', [carController::class, 'create'])->middleware('verified');
-Route::post('cars', [carController::class, 'store'])->name('car.store')->middleware('verified');
-Route::get('cars', [CarController::class, 'index'])->name('cars.index')->middleware('verified');
-Route::get('cars/{id}', [CarController::class, 'edit'])->name('cars.edit')->whereNumber('id')->middleware('verified');
-Route::post('cars/{id}', [CarController::class, 'update'])->name('cars.update')->middleware('verified');
-Route::get('cars/show/{id}', [CarController::class, 'show'])->name('cars.show')->middleware('verified');
-Route::get('cars/destroy/{id}', [CarController::class, 'destroy'])->name('cars.destroy')->middleware('verified');
-Route::get('cars/trashed', [CarController::class, 'showDeleted'])->name('cars.showDeleted')->middleware('verified');
-Route::patch('cars/restore', [CarController::class, 'restore'])->name('cars.restore')->middleware('verified');
-Route::delete('cars/trashed/delete', [CarController::class, 'force_delete'])->name('cars.force_delete')->middleware('verified');
+
+Route::group(
+    [
+        'prefix' => LaravelLocalization::setLocale(),
+        'middleware' => [ 'localeSessionRedirect', 'localizationRedirect', 'localeViewPath' ]
+    ], function(){ 
+        Route::get('cars/create', [carController::class, 'create'])->middleware('verified');
+        Route::post('cars', [carController::class, 'store'])->name('car.store')->middleware('verified');
+        Route::get('cars', [CarController::class, 'index'])->name('cars.index')->middleware('verified');
+        Route::get('cars/{id}', [CarController::class, 'edit'])->name('cars.edit')->whereNumber('id')->middleware('verified');
+        Route::post('cars/{id}', [CarController::class, 'update'])->name('cars.update')->middleware('verified');
+        Route::get('cars/show/{id}', [CarController::class, 'show'])->name('cars.show')->middleware('verified');
+        Route::get('cars/destroy/{id}', [CarController::class, 'destroy'])->name('cars.destroy')->middleware('verified');
+        Route::get('cars/trashed', [CarController::class, 'showDeleted'])->name('cars.showDeleted')->middleware('verified');
+        Route::patch('cars/restore', [CarController::class, 'restore'])->name('cars.restore')->middleware('verified');
+        Route::delete('cars/trashed/delete', [CarController::class, 'force_delete'])->name('cars.force_delete')->middleware('verified');
+    });
+
